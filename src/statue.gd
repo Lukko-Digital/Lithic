976f3e869d6a85c366ba4b41e4790cab358e628e
Ian Lum <ianlum314@gmail.com>
2024-02-13 19:59:11 -0500

    create distinct statues from base statue class

--- src/statue.gd
+++ src/statue.gd
@@ -1,20 +1,20 @@
 extends Area2D
 class_name Statue
 
-@export var condition: Array[StatueCondition]
+@export var condition: Array[StatueCondition] = Array()
 
 @onready var ray: RayCast2D = $RayCast2D
 
 
 func _ready():
-	position = position.snapped(Vector2.ONE * Globals.TILE_SIZE)
-	position += Vector2.ONE * Globals.TILE_SIZE/2
+	get_parent().position = get_parent().position.snapped(Vector2.ONE * Globals.TILE_SIZE)
+	get_parent().position += Vector2.ONE * Globals.TILE_SIZE/2
 
 
 func move(dir: Vector2) -> bool:
 	ray.target_position = dir * Globals.TILE_SIZE
 	ray.force_raycast_update()
 	if !ray.is_colliding():
-		position += dir * Globals.TILE_SIZE
+		get_parent().position += dir * Globals.TILE_SIZE
 		return true
 	else: return false
