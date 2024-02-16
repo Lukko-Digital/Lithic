b4c11f044527e4c283d3e746fbe605b7062365de
mrSquidee <mr.squidy78@gmail.com>
2024-02-16 09:13:38 -0500

    redid snapping with inscene grid snap

--- src/statue_base_classes/statue.gd
+++ src/statue_base_classes/statue.gd
@@ -17,9 +17,9 @@ class_name Statue
 
 @onready var ray: RayCast2D = $RayCast2D
 
-func _ready():
-	get_parent().position = get_parent().position.snapped(Vector2.ONE * Globals.TILE_SIZE)
-	get_parent().position += Vector2.ONE * Globals.TILE_SIZE/2
+# func _ready():
+# 	get_parent().position = get_parent().position.snapped(Vector2.ONE * Globals.TILE_SIZE)
+# 	get_parent().position += Vector2.ONE * Globals.TILE_SIZE/2
 
 
 func move(dir: Vector2) -> bool:
