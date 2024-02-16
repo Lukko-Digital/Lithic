3bd7c49750e60cbc67ce6d3c9ad74cada5074597
Ian Lum <ianlum314@gmail.com>
2024-02-15 22:43:08 -0500

    dialogue signals transmit lines properly

--- src/statue_base_classes/interactable.gd
+++ src/statue_base_classes/interactable.gd
@@ -3,17 +3,17 @@ class_name Interactable
 
 @export_file var dialogue_file
 
-const BRANCH_FLAGS = [
-	"condition_unmet",
-	"no_english",
-	"not_in_tree",
-	"next_to_sign",
-	"unfriendly",
-	"neighbor_language_barrier",
-	"neighbor_condition_unmet",
-	"other_tree_condition_unmet",
-	"solution"
-]
+const BRANCH_FLAGS = {
+	Globals.DialogueState.CONDITIONS_UNMET: "condition_unmet",
+	Globals.DialogueState.NO_ENGLISH: "no_english",
+	Globals.DialogueState.NOT_IN_TREE: "not_in_tree",
+	Globals.DialogueState.NEXT_TO_SIGN: "next_to_sign",
+	Globals.DialogueState.UNFRIENDLY: "unfriendly",
+	Globals.DialogueState.NEIGHBOR_LANGUAGE_BARRIER: "neighbor_language_barrier",
+	Globals.DialogueState.NEIGHBOR_CONDITION_UNMET: "neighbor_condition_unmet",
+	Globals.DialogueState.OTHER_TREE_CONDITION_UNMET: "other_tree_condition_unmet",
+	Globals.DialogueState.SOLUTION: "solution"
+}
 
 
 # Type: Dictionary[String, Dictionary[int, Array[String]]]
@@ -24,6 +24,7 @@ var interaction_count: Dictionary
 func _ready():
 	load_dialogue()
 	init_interaction_count()
+	print(dialogue_tree)
 
 
 func load_dialogue():
@@ -39,7 +40,7 @@ func load_dialogue():
 		match line[0]:
 			"#":
 				var flag = line.substr(1)
-				if flag in BRANCH_FLAGS:
+				if flag in BRANCH_FLAGS.values():
 					branch = flag
 					dialogue_tree[branch] = {}
 			"!":
@@ -54,3 +55,10 @@ func load_dialogue():
 func init_interaction_count():
 	for branch in dialogue_tree.keys():
 		interaction_count[branch] = 0
+
+
+func say(state: Globals.DialogueState):
+	var branch = BRANCH_FLAGS[state]
+	var interaction = interaction_count[branch]
+	var lines = dialogue_tree[branch][interaction]
+	Globals.start_dialogue.emit(lines)
--- src/ui/dialogue_ui.gd
+++ src/ui/dialogue_ui.gd
@@ -2,12 +2,37 @@ extends Control
 
 @export_file var dialogue_file
 
-@onready var label: Label = $Label
+@onready var label: Label = $TextBox/Label
 @onready var text_timer: Timer = $TextTimer
 
-var game_script
+var display_in_progress = false
+var dialogue_lines
+var current_line
 
 const TEXT_SPEED = 0.04
+
+func _ready():
+	Globals.start_dialogue.connect(_start_dialogue)
+
+
+func _start_dialogue(lines):
+	dialogue_lines = lines
+	current_line = -1
+	show()
+	next_line()
+
+
+func next_line():
+	if display_in_progress:
+		display_in_progress = false
+		label.visible_characters = len(label.text)
+	else:
+		current_line += 1
+		if current_line >= len(dialogue_lines):
+			hide()
+		else:
+			label.text = dialogue_lines[current_line]
+
 
 func animate_display():
 	label.visible_characters = 0
