bf00f4eb2ad8998f7d91ff96a79dc86160c36951
Ian Lum <ianlum314@gmail.com>
2024-02-16 13:16:28 -0500

    can change portraits through dialogue

--- src/ui/dialogue_ui.gd
+++ src/ui/dialogue_ui.gd
@@ -9,11 +9,23 @@ var display_in_progress = false
 var dialogue_lines
 var current_line
 
+var active_portrait: String
+var portrait_map: Dictionary
+
 const TEXT_SPEED = 0.04
 
 func _ready():
 	Globals.start_dialogue.connect(_start_dialogue)
 	Globals.advance_dialogue.connect(next_line)
+	init_portraits()
+	print(portrait_map)
+
+
+func init_portraits():
+	for child in $TextBox/Portraits.get_children():
+		portrait_map[child.name] = child
+	active_portrait = portrait_map.keys()[0]
+	portrait_map[active_portrait].show()
 
 
 func _start_dialogue(lines):
@@ -33,8 +45,18 @@ func next_line():
 		if current_line >= len(dialogue_lines):
 			exit_dialogue()
 		else:
-			label.text = dialogue_lines[current_line]
-			animate_display()
+			show_line()
+
+
+func show_line():
+	var portrait = dialogue_lines[current_line].get_slice(": ", 0)
+	var line = dialogue_lines[current_line].get_slice(": ", 1)
+	if portrait != active_portrait:
+		portrait_map[active_portrait].hide()
+		active_portrait = portrait
+		portrait_map[active_portrait].show()
+	label.text = line
+	animate_display()
 
 
 func animate_display():
