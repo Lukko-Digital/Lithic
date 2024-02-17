39d93cfea225d23b6c975911865d3319d9c0a8d9
Ian Lum <ianlum314@gmail.com>
2024-02-17 14:46:54 -0500

    ui handles all scene transitions add transition fades

--- src/narrator_scene.gd
+++ src/narrator_scene.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
-@export var next_scene: PackedScene
 
 func _ready():
 	Globals.end_dialogue.connect(_transition)
 
+
 func _transition():
-	get_tree().change_scene_to_packed(next_scene)
+	$ui.transition_scene(true)
--- src/ui/ui.gd
+++ src/ui/ui.gd
@@ -2,3 +2,17 @@ extends CanvasLayer
 
 @export var door_code: Array[int]
 @export var next_scene: PackedScene
+
+@onready var transition_player: AnimationPlayer = $ScreenColor/AnimationPlayer
+
+func _ready():
+	transition_player.play("fade_from_black")
+
+
+func transition_scene(hard_cut=false):
+	if not hard_cut:
+		transition_player.play("fade_to_black")
+		await transition_player.animation_finished
+	Globals.in_door_ui = false
+	Globals.in_dialogue = false
+	get_tree().change_scene_to_packed(next_scene)
