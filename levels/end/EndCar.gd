87f60b815d9a8b32f58c00f38767cb37ed662228
mrSquidee <mr.squidy78@gmail.com>
2024-02-17 23:20:10 -0500

    end scene

--- levels/end/EndCar.gd
+++ levels/end/EndCar.gd
@@ -1,6 +1,14 @@
 extends StaticBody2D
 
+@export var ui: CanvasLayer
+
+func _ready():
+	Globals.end_dialogue.connect(transition_scene)
+
 ## Called when player interacts with car
 func interact():
 	get_parent().say(Globals.DialogueState.SOLUTION)
 
+
+func transition_scene():
+	ui.transition_scene(true)
--- levels/end/end_player.gd
+++ levels/end/end_player.gd
@@ -1,16 +1,18 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 40.0
 
 @onready var ray: RayCast2D = $RayCast2D
 @onready var audio_player: AnimationPlayer = $AudioStreamPlayer/AudioAnimationPlayer
-@onready var sprite: AnimatedSprite2D = 
+@onready var sprite: AnimatedSprite2D = $Sprite
+@onready var interact_prompt: AnimatedSprite2D = $CanvasLayer/InteractPrompt
 
 func _physics_process(delta):
 	var direction = Vector2(
 		Input.get_axis("left", "right"), Input.get_axis("up", "down")
 	).normalized()
+
 	handle_animation(direction)
 	velocity = direction * SPEED
 	move_and_slide()
@@ -18,26 +20,26 @@ func _physics_process(delta):
 	if !direction.is_zero_approx():
 		ray.target_position = direction * 30
 
+	handle_interact_prompt()
+
 func handle_animation(direction: Vector2):
 	match direction.round():
 		Vector2():
-			animation_player.play("idle")
+			sprite.play("idle_" + sprite.animation)
 		Vector2(1,-1), Vector2(1,0), Vector2(1,1):
 			# right
-			animation_player.play("run_left")
-			sprite.flip_h = true
+			sprite.play("right")
+			sprite.flip_h = false
 		Vector2(-1,-1), Vector2(-1,0), Vector2(-1,1):
 			# left
-			animation_player.play("run_left")
-			sprite.flip_h = false
+			sprite.play("right")
+			sprite.flip_h = true
 		Vector2(0,-1):
 			# up
-			animation_player.play("run_up")
+			sprite.play("up")
 		Vector2(0,1):
 			# down
-			animation_player.play("run_down")
-	if direction != Vector2():
-		walk_sound.play()
+			sprite.play("down")
 
 
 func _unhandled_input(event):
@@ -57,3 +59,25 @@ func handle_interact():
 	var colliding = ray.get_collider()
 	if colliding.is_in_group("statue"):
 		colliding.interact()
+
+
+func handle_interact_prompt():
+	if facing_interactable():
+		interact_prompt.show()
+		interact_prompt.play("default")
+	else:
+		interact_prompt.hide()
+		interact_prompt.stop()
+
+
+func facing_interactable():
+	if Globals.in_dialogue or Globals.in_door_ui:
+		return false
+
+	var colliding = ray.get_collider()
+	if !is_instance_valid(colliding):
+		return false
+	elif colliding.is_in_group("statue"):
+		return true
+	
+	return false
